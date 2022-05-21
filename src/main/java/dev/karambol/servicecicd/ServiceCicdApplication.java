0c5a6eb59556cd4b8e56dfa90428b5003b2126ef
dejano <dejano.with.tie@gmail.com>
2022-05-21 11:13:05 +0200

    init

---
+++ src/main/java/dev/karambol/servicecicd/ServiceCicdApplication.java
@@ -0,0 +1,13 @@
+package dev.karambol.servicecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/karambol/servicecicd/ServiceCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.karambol.servicecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
